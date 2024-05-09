c7ec5acf13103f49c52236500bc154b0163b5d3f
FabioGMarascia <fabiogaspare.marascia@gmail.com>
2024-05-09 15:57:56 +0200

    Snack - 1/2/3/4

---
+++ js/script.js
@@ -0,0 +1,70 @@
+// // SNACK - 1
+// let zucchine = [
+// 	{ peso: 10, lunghezza: 5 },
+// 	{ peso: 7, lunghezza: 5 },
+// 	{ peso: 8, lunghezza: 5 },
+// 	{ peso: 3, lunghezza: 5 },
+// 	{ peso: 1, lunghezza: 5 },
+// 	{ peso: 9, lunghezza: 5 },
+// 	{ peso: 15, lunghezza: 5 },
+// 	{ peso: 11, lunghezza: 5 },
+// 	{ peso: 17, lunghezza: 5 },
+// 	{ peso: 21, lunghezza: 5 },
+// ];
+
+// for (const element of zucchine) {
+// 	pesoTotale += element.peso;
+// }
+// console.log(`IL PESO TOTALE È ${pesoTotale}`);
+
+// // SNACK - 2
+// function numbMagMin(array, i, chiave) {
+// 	let elementiMinori = [];
+// 	let elementiMaggiori = [];
+// 	let sommaTotaleMinori = 0;
+// 	let sommaTotaleMaggiori = 0;
+// 	for (const element of array) {
+// 		let valoreElemento = element[chiave];
+// 		if (valoreElemento < i) {
+// 			elementiMinori.push(valoreElemento);
+// 			sommaTotaleMinori += valoreElemento;
+// 		} else {
+// 			elementiMaggiori.push(valoreElemento);
+// 			sommaTotaleMaggiori += valoreElemento;
+// 		}
+// 	}
+// 	console.log(`Array minori di ${i}: ${elementiMinori}`);
+// 	console.log(`Somma minori: ${sommaTotaleMinori}`);
+// 	console.log(`Array maggiori di ${i}: ${elementiMaggiori}`);
+// 	console.log(`Somma maggiori: ${sommaTotaleMaggiori}`);
+// }
+// numbMagMin(zucchine, 15, `peso`);
+
+// // SNACK - 3
+// let a = `ciao`;
+
+// function reverseString(stringa) {
+// 	let array1 = Array.from(stringa);
+// 	let array2 = [];
+// 	array1.reverse();
+// 	console.log(array1);
+// 	array2 = array1.join("");
+// 	console.log(array2);
+// }
+// reverseString(a);
+
+// SNACK - 4
+// const array1 = [`a`, `b`, `c`];
+// const array2 = [1, 2, 3];
+
+// function arrayFusion(x, y) {
+// 	let arrayFused = [];
+// 	for (let i = 0; i < n; i++) {
+// 		arrayFused.push(x[i]);
+// 		arrayFused.push(y[i]);
+// 	}
+// 	console.log(arrayFused);
+// }
+// arrayFusion(array1, array2);
+
+// SNACK - 5
